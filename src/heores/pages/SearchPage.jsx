a95ffe531efa9228f560c6678df42f10ed6fcbb6
unknown <emprenderusinque@hotmail.com>
2022-10-27 14:43:34 -0500

    show message in SearchPage added

--- src/heores/pages/SearchPage.jsx
+++ src/heores/pages/SearchPage.jsx
@@ -16,15 +16,16 @@ export const SearchPage = () => {
   const { searchText, onInputChange } = useForm({
     searchText: q
   });
-  
+
   const heroes = getHeroesByName( q );
 
-  console.log(heroes)
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
 
   const onSearchSubmit = ( event ) => {
     event.preventDefault();
 
-    if( searchText.trim().length <= 2  ) return alert('Introduce algo') ;
+    if( searchText.trim().length <= 1  ) return alert('Introduce algo') ;
 
     navigate(`?q=${ searchText }`);
     
@@ -61,14 +62,27 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr/>
-{/* 
-          <div className="alert alert-primary">
-            Search a hero
-          </div>
-          <div className="alert alert-danger">
-            No hero With <b> ABC </b>
-          </div> */}
 
+          {/* {
+            ( q === '' ) 
+            ? <div className="alert alert-primary"> Search a hero </div>
+            : ( heroes.length === 0 ) 
+              && <div className="alert alert-danger"> No hero With <b> { q } </b></div> 
+          } */}
+
+          <div 
+            className="alert alert-primary animate__animated animate__fadeIn" 
+            style={{ display: showSearch ? '' : 'none'}}
+          > 
+            Search a hero 
+          </div>
+
+          <div 
+            className="alert alert-danger animate__animated animate__fadeIn" 
+            style={{ display: showError ? '' : 'none'}}
+          > 
+            No hero With <b> { q } </b>
+          </div> 
           {
             heroes.map( hero => (
                 <HeroCard key={ hero.id} {...hero}/> 
