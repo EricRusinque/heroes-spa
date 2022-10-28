bdd702df59b956c032a493dfdfca2a4f2616d5f4
unknown <emprenderusinque@hotmail.com>
2022-10-28 14:46:16 -0500

    PublicRoute implemented

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,20 +3,26 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { HeroesRoutes } from '../heores'
 import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
            
-            <Route path='login' element={ <LoginPage />}/>
+            <Route path='login' element={
+              <PublicRoute>
+                 <LoginPage />
+              </PublicRoute>
+            }/>
+
+
 
             <Route path='/*' element={
               <PrivateRoute>
                 <HeroesRoutes/>
               </PrivateRoute>
             }/>
-
 
         </Routes>
     </>
